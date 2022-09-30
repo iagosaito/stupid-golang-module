9523e08912f21bbf1a2071960644dc73a05cc2a7
Iago Saito <iagosaito1999@gmail.com>
2022-09-30 19:12:07 -0300

    add go docs for stupid package

--- stupid-package/stupid-phases.go
+++ stupid-package/stupid-phases.go
@@ -1,5 +1,11 @@
+// Stupid package that provide just stupid things
 package stupidpackage
 
+// Provide the stupid sentence:
+//
+// - "Sometimes when I close my eyes, I can’t see."
+//
+// That's stupid, isn't it?
 func SaySomethingStupid() string {
 	return "Sometimes when I close my eyes, I can’t see."
 }
